d36f6b4103fd87d66aabe1c02ea4804b54dbdf1f
ngocbv <ngocbv@remitano.com>
2021-11-20 19:16:03 +0700

    Temporary remove Balances and Orders

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -159,12 +159,12 @@ export default function TopBar() {
           <Menu.Item key={tradePageUrl} style={{ margin: '0 10px 0 20px' }}>
             TRADE
           </Menu.Item>
-          {connected && (!searchFocussed || location.pathname === '/balances') && (
+          {false && connected && (!searchFocussed || location.pathname === '/balances') && (
             <Menu.Item key="/balances" style={{ margin: '0 10px' }}>
               BALANCES
             </Menu.Item>
           )}
-          {connected && (!searchFocussed || location.pathname === '/orders') && (
+          {false && connected && (!searchFocussed || location.pathname === '/orders') && (
             <Menu.Item key="/orders" style={{ margin: '0 10px' }}>
               ORDERS
             </Menu.Item>
