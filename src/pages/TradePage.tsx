7d79bcd8a6ce98f3c587c1420171e9d52c58948b
ngocbv <ngocbv@remitano.com>
2021-11-20 17:44:24 +0700

    Temporary remove chart

--- src/pages/TradePage.tsx
+++ src/pages/TradePage.tsx
@@ -341,14 +341,17 @@ const RenderNormal = ({ onChangeOrderRef, onPrice, onSize }) => {
         flexWrap: 'nowrap',
       }}
     >
-      <Col flex="auto" style={{ height: '50vh' }}>
-        <Row style={{ height: '100%' }}>
-          <TVChartContainer />
-        </Row>
-        <Row style={{ height: '70%' }}>
-          <UserInfoTable />
-        </Row>
-      </Col>
+      {
+        // <Col flex="auto" style={{ height: '50vh' }}>
+        // <Row style={{ height: '100%' }}>
+        //   <TVChartContainer />
+        // </Row>
+        // <Row style={{ height: '70%' }}>
+        //   <UserInfoTable />
+        //   </Row>
+        // </Col>
+      }
+      <div>sdjflksdfjkl</div>
       <Col flex={'360px'} style={{ height: '100%' }}>
         <Orderbook smallScreen={false} onPrice={onPrice} onSize={onSize} />
         <TradesTable smallScreen={false} />
@@ -367,9 +370,11 @@ const RenderNormal = ({ onChangeOrderRef, onPrice, onSize }) => {
 const RenderSmall = ({ onChangeOrderRef, onPrice, onSize }) => {
   return (
     <>
-      <Row style={{ height: '30vh' }}>
-        <TVChartContainer />
-      </Row>
+      {
+      //   <Row style={{ height: '30vh' }}>
+      //   <TVChartContainer />
+      // </Row>
+      }
       <Row
         style={{
           height: '900px',
@@ -406,9 +411,11 @@ const RenderSmall = ({ onChangeOrderRef, onPrice, onSize }) => {
 const RenderSmaller = ({ onChangeOrderRef, onPrice, onSize }) => {
   return (
     <>
-      <Row style={{ height: '50vh' }}>
-        <TVChartContainer />
-      </Row>
+      {
+      // <Row style={{ height: '50vh' }}>
+      //   <TVChartContainer />
+      // </Row>
+      }
       <Row>
         <Col xs={24} sm={12} style={{ height: '100%', display: 'flex' }}>
           <TradeForm style={{ flex: 1 }} setChangeOrderRef={onChangeOrderRef} />
