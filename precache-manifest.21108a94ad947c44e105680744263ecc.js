f00994b65978e517049b74aa66a74fad549b13a5
ngocbv <ngocbv@remitano.com>
2021-11-21 14:08:30 +0700

    Updates

---
+++ precache-manifest.21108a94ad947c44e105680744263ecc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74af52cabc3f24739add55fe8cc4ebc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef9007ff8f213835fccf",
+    "url": "/static/css/2.3a0ac4c9.chunk.css"
+  },
+  {
+    "revision": "fde3255443bf41a40c87",
+    "url": "/static/css/main.e69d9291.chunk.css"
+  },
+  {
+    "revision": "ef9007ff8f213835fccf",
+    "url": "/static/js/2.6fbc9d05.chunk.js"
+  },
+  {
+    "revision": "958608aaef3e81c9b20a22457416f406",
+    "url": "/static/js/2.6fbc9d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde3255443bf41a40c87",
+    "url": "/static/js/main.b415a788.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "a938fe5e27c00512cb2aeb9206609076",
+    "url": "/static/media/logo.a938fe5e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pandadex/precache-manifest.f5252b542d9981985486354e4a132a18.js"
+  "/precache-manifest.21108a94ad947c44e105680744263ecc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pandadex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
